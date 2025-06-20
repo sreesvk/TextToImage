c1320f202597de85bf0411f91b473e8b4dfbb255
SREERAM V KUMAR <31160598+sreesvk@users.noreply.github.com>
2025-06-20 12:52:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import TextToImage from './Component/TextToImage';
+import TxtToImg from './Component/TxtToImg';
+import Sample from './Component/Sample';
+import Texty from './Component/Texty';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+
+  <Texty/>  
+  </>
+    
   );
 }
 
